5f18efad997d9844af7d7920b97b5a0982be6f90
tj <anaz.thaj@gmail.com>
2023-08-12 16:28:17 +0530

    refactor: comments

--- plugins/simple-contact-form/simple-contact-form.php
+++ plugins/simple-contact-form/simple-contact-form.php
@@ -9,34 +9,34 @@
  */
 
 if(!defined('ABSPATH')) {
-    echo 'what yu tryin to do?';
-    exit;
+  echo 'what yu tryin to do?';
+  exit;
 }
 
 class SimpleContactForm
 {
-    public function __construct()
-    {
-        add_action('init', array($this, 'create_custom_post_type'));
-    }
+  public function __construct()
+  {
+    add_action('init', array($this, 'create_custom_post_type'));
+  }
 
-    public function create_custom_post_type()
-    {
-        $args = array(
-        'public' => true,
-        'has_archive' => true,
-        'supports' => array('title'),
-        'exclude_from_search' => true,
-        'publicly_queryable' => false,
-        'capability' => 'manage_options',
-        'labels' => array(
+  public function create_custom_post_type()
+  {
+    $args = array(
+      'public' => true,
+      'has_archive' => true,
+      'supports' => array('title'),
+      'exclude_from_search' => true,
+      'publicly_queryable' => false,
+      'capability' => 'manage_options',
+      'labels' => array(
         'name' => 'Contact Form',
         'singular_name' => 'Contact Form Entry',
-        ),
-        'menu_icon' => 'dashicons-media-text',
-        );
-        register_post_type('simple-contact-form', $args);
-    }
+      ),
+      'menu_icon' => 'dashicons-media-text',
+    );
+    register_post_type('simple-contact-form', $args);
+  }
 }
 
 new SimpleContactForm;
